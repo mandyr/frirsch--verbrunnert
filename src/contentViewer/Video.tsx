d5d0c9bf8feca4c32141b0f559e34153d2815499
Mandy <dev@mandyr.de>
2021-05-24 22:20:38 +0200

    add video (to be replaced)

--- src/contentViewer/Video.tsx
+++ src/contentViewer/Video.tsx
@@ -1,3 +1,22 @@
 export function Video(props: any) {
-  return <div>Wir haben 1 nices video für euch gedreht</div>;
+  return (
+    <div
+      style={{
+        width: '100%',
+      }}
+    >
+      <p>Wir haben 1 nices video für euch gedreht</p>
+      <iframe
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+        src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 }
