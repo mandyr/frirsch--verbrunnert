1971170914a290b4dc8be9c2ef4fbd351efbdd9c
Mandy <dev@mandyr.de>
2021-05-26 19:28:50 +0200

    add background image

--- src/contentViewer/Home.tsx
+++ src/contentViewer/Home.tsx
@@ -3,11 +3,11 @@ import welcome from "../popUps/welcome.gif";
 
 export function Home(props: any) {
   return (
-    <div className="home"> 
+    <div className="home">
       <h1>Willkommen bei frisch-verbrunnert</h1>
       <img src={welcome} alt="welcome to my crib"></img>
-      <p>In meiner kleinen Ecken im WWW gibt es viel zu entdecken!</p>
-      <Marquee text={"Wir wünschen alles, alles gute zur Hochzeit!!!"} />
+      <p>In unserer kleinen Ecke im WWW gibt es viel zu entdecken!</p>
+      <Marquee text={'Wir wünschen alles, alles gute zur Hochzeit!!!'} />
     </div>
   );
 }
--- src/sidebar/Sidebar.tsx
+++ src/sidebar/Sidebar.tsx
@@ -1,13 +1,11 @@
 import { px } from '../popUps/PopUpWindow';
 import schweinkram from '../popUps/schweinkram.gif';
-import twinkle from '../popUps/twinkle.gif';
 import './Sidebar.css';
 
 export function SideBar() {
   return (
     <div className="sideBar">
-      <img style={{ width: px(300)}} src={schweinkram} alt="schweinkram"></img>
-      <img  style={{ width: px(100)}} src={twinkle} alt="twinkle bitch"></img>
+      <img style={{ width: px(300) }} src={schweinkram} alt="schweinkram"></img>
     </div>
   ); 
 }
