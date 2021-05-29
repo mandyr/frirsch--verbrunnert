8284d1efc53aa7eca331f41714f8e35abd4944c4
Mandy <dev@mandyr.de>
2021-05-29 18:42:39 +0200

    added video

--- src/contentViewer/Video.tsx
+++ src/contentViewer/Video.tsx
@@ -1,22 +1,15 @@
+import downloadButton from "./download-button.jpg";
+
 export function Video(props: any) {
   return (
     <div
       style={{
-        width: '100%',
+        width: "100%",
       }}
     >
       <p>Wir haben 1 nices video für euch gedreht</p>
-      <iframe
-        style={{
-          width: '100%',
-          height: '100%',
-        }}
-        src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      <a download href="/video/video.mp4"><img style={{ width: "20%"}}src={downloadButton} alt="Hier Downloaden!"/></a>
+
     </div>
   );
 }
